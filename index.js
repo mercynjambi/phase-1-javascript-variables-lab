1b197d6f9f7a3b6e268e1ec56d51e5b69043d8f1
mercynjambi <njambim695@gmail.com>
2024-03-25 19:01:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+
+ const companyName1 = 'Scuber';
+
+ let mostProfitableNeighborhood = 'Chelsea';
+
+ let companyCeo = 'Susan Smith';
+ 
